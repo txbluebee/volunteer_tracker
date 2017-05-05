b5b0edc778ee79f692d57f08be3fabb0c4889651
Brian Fan <kuomu.fan@yahoo.com>
2017-05-05 08:50:39 -0700

    adds ==, save, .all methods to Volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,8 +6,24 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
   end
 
+  def ==(another_volunteer)
+    (self.name() == another_volunteer.name()) && (self.project_id == another_volunteer.project_id())
+  end
 
+  def self.all
+    volunteers_in_db = DB.exec("SELECT * FROM volunteers;")
+    all_volunteers = []
+    volunteers_in_db.each() do |volunteer|
+      name = volunteer.fetch('name')
+      project_id = volunteer.fetch('project_id').to_i()
+      each_volunteer = Volunteer.new({:name => name, :project_id => project_id})
+      all_volunteers.push(each_volunteer)
+    end
+    all_volunteers
+  end
 
-
+  def save
+    DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id});")
+  end
 
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -8,5 +8,27 @@ describe(Volunteer) do
     end
   end
 
+  describe('#==') do
+    it('is equal to another volunteer with same name and project_id') do
+      test_volunteer = Volunteer.new({:name => 'Brian', :project_id => 1})
+      test_volunteer2 = Volunteer.new({:name => 'Brian', :project_id => 1})
+      expect(test_volunteer).to(eq(test_volunteer2))
+    end
+  end
 
+  describe('.all') do
+    it('returns the array of all volunteers') do
+      expect(Volunteer.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the volunteer to the array of all volunteers') do
+      test_volunteer = Volunteer.new({:name => 'Brian', :project_id => 1})
+      test_volunteer.save()
+      test_volunteer2 = Volunteer.new({:name => 'Brian', :project_id => 2})
+      test_volunteer2.save()
+      expect(Volunteer.all).to(eq([test_volunteer, test_volunteer2]))
+    end
+  end
 end
