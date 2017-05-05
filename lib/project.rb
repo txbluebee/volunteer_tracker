bac3fa4b02ed0727b20ab68b68e0b8b9d3b701a4
Brian Fan <kuomu.fan@yahoo.com>
2017-05-05 09:15:29 -0700

    adds .all, save, and id method to Project class and passs unit specs

--- lib/project.rb
+++ lib/project.rb
@@ -9,4 +9,22 @@ class Project
   def ==(another_project)
     (self.id() == another_project.id()) && (self.description() == another_project.description())
   end
+
+  def self.all
+    projects_in_db = DB.exec("SELECT * FROM projects;")
+    all_projects = []
+    projects_in_db.each() do |project|
+      id = project.fetch('id').to_i()
+      description = project.fetch('description')
+      all_projects.push(Project.new(:id => id, :description => description))
+    end
+    all_projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (description) VALUES ('#{@description}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -15,4 +15,27 @@ describe(Project) do
       expect(test_project).to(eq(test_project2))
     end
   end
+
+  describe('.all') do
+    it('returns the array of all projects') do
+      expect(Project.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the project to the array of all projects') do
+      test_project = Project.new({:description => "Clean Up Seattle", :id => nil})
+      test_project.save()
+      expect(Project.all).to(eq([test_project]))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id for the project') do
+      test_project = Project.new({:description => "Clean Up Seattle", :id => nil})
+      test_project.save()
+      expect(test_project.id()).to(be_instance_of(Fixnum))
+    end
+  end
+
 end
